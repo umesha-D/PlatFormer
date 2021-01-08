ceadb742e2f76c598710a46d354ae01979a42086
umesha-D <umeshadsamanayaka@gmail.com>
2021-01-08 23:43:14 +0530

    second commit

--- new.py
+++ new.py
@@ -21,12 +21,55 @@ def draw_grid():
         pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
 
 
+draw_grid()
+
+
+class Player():
+    def __init__(self, x, y):
+        img = pygame.image.load('m22.png')
+        self.image = pygame.transform.scale(img, (40, 80))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.vel_y = 0
+        self.jumped = False
+
+    def update(self):
+        dx = 0
+        dy = 0
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE] and self.jumped == False:
+            self.vel_y = -15
+            self.jumped = True
+        if key[pygame.K_SPACE] == False:
+            self.jumped = False
+        if key[pygame.K_LEFT]:
+            dx -= 5
+        if key[pygame.K_RIGHT]:
+            dx += 5
+
+        self.vel_y += 1
+        if self.vel_y > 10:
+            self.vel_y = 10
+        dy += self.vel_y
+
+        self.rect.x += dx
+        self.rect.y += dy
+
+        if self.rect.bottom > screen_height:
+            self.rect.bottom = screen_height
+            dy = 0
+
+        screen.blit(self.image, self.rect)
+
+
 class World():
     def __init__(self, data):
         self.tile_list = []
 
-        dirt_img = pygame.image.load('grass3.png')
-        grass_img = pygame.image.load('grass3.png')
+        dirt_img = pygame.image.load('dirt2.png')
+        grass_img = pygame.image.load('dirt2.png')
 
         row_count = 0
         for row in data:
@@ -58,21 +101,21 @@ world_data = [
 
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 2, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 8, 1],
+    [1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 2, 2, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 1],
-    [1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 1],
-    [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 2, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 1],
+    [1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 2, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 2, 2, 2, 2, 6, 6, 6, 1, 1],
+    [1, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
+    [1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 
 ]
-
+player = Player(100, screen_height - 130)
 world = World(world_data)
 
 run = True
@@ -82,7 +125,7 @@ while True:
 
     world.draw()
 
-    draw_grid()
+    player.update()
 
     print(world.tile_list)
 
